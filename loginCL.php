95593345b1a1c9652ea23ca98f53d314b5e907c2
qims-png <2024247272@student.uitm.edu.my>
2026-06-05 17:36:59 +0800

    update modul

--- loginCL.php
+++ loginCL.php
@@ -1,12 +1,28 @@
 <?php
 session_start();
+include("database.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    
-    $_SESSION['client_id'] = "C001"; 
-    $_SESSION['client_name'] = "Demo Client"; 
-    header("Location: client.php");
-    exit();
+    $clientID= $_POST['client_id'];
+    $password= $_POST['password'];
+
+    if($password === "7890"){
+        $query="SELECT * FROM client WHERE Client_ID='$clientID'";
+        $result= mysqli_query($conn,$query);
+
+        if (mysqli_num_rows($result)==1){
+            $row = mysqli_fetch_assoc($result);
+            $_SESSION['client_id']= $row['Client_ID'];
+            $_SESSION['username']= $row['Client_Name'];
+            header("Location: client.php");
+            exit();
+        }else {
+            $error = "Client is not exist!";
+        }
+    }else{
+            $error = "Wrong Password!";
+        
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -18,10 +34,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <h2>Login Client</h2>
     <form method="post">
         <label>Client ID:</label><br>
-        <input type="text" name="username" required><br>
+        <input type="text" name="client_id" required><br>
         <label>Password:</label><br>
         <input type="password" name="password" required><br><br>
         <input type="submit" value="Login">
     </form>
+    <?php if(isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
 </body>
 </html>
